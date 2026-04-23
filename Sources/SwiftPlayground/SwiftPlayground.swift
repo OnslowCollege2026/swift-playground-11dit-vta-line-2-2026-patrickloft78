3951d8be46bfeded042945bf74e21deddda25721
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-04-23 03:08:22 +0000

    yeah

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,16 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        print("Onslow College") 
+        print("p.l")
+        print("ultra 4k screen display")
+        let firstname = ("patrick owen loft")
+        print (firstname)
+        var control_variable = 0
+        let adjective1 = "confused"
+        let noun1 =  "biscut"
+        let verb1 = "man"
+        let madlibstring = "Once upon a time the \(adjective1) \(noun1) \(verb1) went to his cookie jar, instead of a cookie he found a half eaten \(noun1) the \(adjective1) \(noun1) \(verb1)screamed in \(adjective1) \(noun1)horror."
+        print (madlibstring)
     }
 }
