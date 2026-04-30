043f38a13bf427e4aa7570640d2c9455f087048f
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-04-30 02:39:05 +0000

    yay

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let x = 10
+        let y = 5
+        let added = x + y 
+        let subtracted = x - y
+        let multiplied = x*y
+        let divided = x / y
+        print("\(x) + \(y) = \(added)")
+        print("\(x) - \(y) = \(subtracted)")  
+        print("\(x) * \(y) = \(multiplied)")
+        print("\(x) / \(y) = \(divided)")  
+       
     }
 }
