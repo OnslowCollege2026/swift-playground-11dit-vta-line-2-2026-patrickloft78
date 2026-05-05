394e1268336d4af9b012d5582445bbb69deb012b
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-05-05 21:02:14 +0000

    May 5, 2026, 9:02 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,6 +5,21 @@ struct SwiftPlayground {
     static func main() {
         let isOldEnough = 16 >= 18
         print(isOldEnough) 
-       
+       print("your name is \(firstname)")
+       print("enter your name")
+       print("Enter your age:")
+
+var userInput = readLine()!
+let age = Int(ageString)!
+
+print("Enter the password:")
+userInput = readLine()!
+
+let isOldEnough = age >= 18
+let knowsSecret = userInput == "halibut"
+let canEnterBar = isOldEnough && knowsSecret
+
+print(canEnterBar)
+
     }
 }
