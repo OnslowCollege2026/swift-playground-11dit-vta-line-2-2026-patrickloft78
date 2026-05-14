4f9e700beb99fb8ea7a9003c3aaac019ef899121
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-05-14 02:16:49 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,24 +4,6 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print ("Welcome to Cinema Tickets.")
-var isRunning = true 
-var totalPrice = 0 
-while isRunning{
-print ("c for child:  $8")
-print ("a for adult:  $12")
-print ("s for senior: $10")
-print ("q for quit")
 
-print ("Please enter a choice: c")
-print ("Child added. Total is: $8")
 
-print ("c for child:  $8")
-print ("a for adult:  $12")
-print ("s for senior: $10")
-print ("q for quit")
-
-print ("Please enter a choice: a")
-print ("Adult added. Total is $20.")
 }
-}}
