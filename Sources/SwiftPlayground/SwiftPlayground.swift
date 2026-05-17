60875c5952ed43b851221369fce298dab0dd6cf7
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-05-17 22:20:13 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,15 @@
 @main
 struct SwiftPlayground {
     static func main() {
-
-
+var invitees: [String] = []
+var adding = true 
+while adding == true {
+print("who do  you want to invite to the party?")
+let invitee = readLine()!
+if invitee == ""{
+    adding = false 
+}else{
+    invitees.append(invitee)
+}
+}
 }
