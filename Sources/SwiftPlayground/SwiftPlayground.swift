be274ae3b0c599fb7e9bfded200c45bdaaa50c49
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-05-26 21:06:04 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,24 +5,4 @@
 struct SwiftPlayground {
     static func main() {
 
-var age: Int? = nil
-
-
-while age == nil {
-    print("Enter your age:")
-    
-    if let input = readLine(), let enteredAge = Int(input) {
-        if enteredAge < 0 {
-            print("Age cannot be negative. Enter a valid age.")
-        } else if enteredAge < 18 {
-            print("You must be 18 or older. Enter a valid age.")
-        } else {
-            age = enteredAge
-        }
-    } else {
-        print("Invalid input. Enter a valid integer for age.")
-    }}
-
-
-print("Valid age entered: \(age!)")
     }}
