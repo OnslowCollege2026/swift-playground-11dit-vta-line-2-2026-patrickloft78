9950df2cbf023a905230ce0c4f9005102ea42a5e
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-05-26 21:00:27 +0000

    May 26, 2026, 9:00 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,49 +4,25 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        var ingredients: [String] = []
-        var isRunning = true
-        while isRunning == true{
-             print("What ingredients do you have?")
-             let input = readLine()!
-             if input == ""{
-                isRunning = false 
-             } 
-             else if input == "chocolate"{
-                print("yum!")
-                
-                }
-             else {
-                ingredients.append(input)
-             }
-             }
-        print("Let's start cooking!")
-print("                 ⣤⣤⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠴⠚⠋⠉⣾⣿⣿⣿⣿⣿⣿⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⢀⡴⣏⠥⠒⠚⠉⠉⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⡄⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⣰⣿⠋⠀⠀⠀⠀⠀⠀⠀⠛⠋⠉⠀⠀⠙⠺⣿⣿⣿⣿⣿⣿⣿⠆⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⡼⠃⠈⢳⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⡿⢯⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⡼⠁⠀⢀⡿⡞⠈⠉⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⡿⠋⠁⠸⡆⠀⠀⠀")
-print("⠀ ⠀⢰⡧⢄⡀⣼⢻⠁⠀⠀⠀⠀⠀⠀⠀⠰⡄⠀⠀⠀⠀⠀⠈⠁⠘⡄⠀⠀⢷⠀⠀⠀")
-print("⠀⠀⢀⣿⠀⠠⡈⠉⠙⠒⠒⡗⠀⠀⠀⠀⠀⠁⡟⢦⡀⠀⠀⠀⠀⠀⠀⠹⡀⠀⠘⢦⡀⠀")
-print(" ⢠⣾⣽⠆⠀⣇⠀⠀⠀⠀⡼⡄⠀⠀⠀⠀⠀⡇⠀⠻⣦⡀⠠⡀⠀⠀⠀⡇⠉⣿⠚⠛⠓")
-print("⠘⠉⠁⢸⡀⠀⠘⣦⣰⣦⣀⣇⠙⣆⠀⠀⠀⠀⡇⠀⠀⠀⠙⠲⡇⠀⠀⡀⢹⠀⡏⠀⠀⠀")
-print("⠀ ⠀⠀⡇⠀⠀⢹⠙⣷⣈⠙⠶⠘⢷⡗⠦⢤⣿⠃⣠⣴⠄⠀⡇⠀⠀⡽⠺⢶⡁⠀⠀⠀")
-print("⠀ ⠀⠀⠹⣴⣄⠀⢧⠈⢙⣷⡦⠀⠀⠀⠀⠀⣴⣿⣋⣀⠀⠀⡟⣀⣼⣁⣄⢸⠁⠀⠀⠀")
-print("⠀⠀⠀⠀⢼⡟⠛⣿⣮⡿⢛⡉⠀⠀⠀⢠⠀⠀⠈⠉⠉⡹⠀⣸⣡⣟⠛⢿⡟⠻⣧⠀⠀⠀")
-print("⠀⠀⠀⠀⠉⠑⠺⣿⣿⣷⣄⡼⠀⠀⢤⡤⠤⢴⡀⠀⠀⢣⡼⠛⢉⣽⠟⠉⠁⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣷⣤⣀⣀⣈⠳⠦⠞⣁⣠⠞⠉⣠⣾⡟⠁⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠈⠻⢧⣽⣿⣿⣿⡿⢯⣀⡴⠋⠛⠁⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⢷⣿⣿⣧⢿⠁⠀⠀⠀⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠷⡿⣟⣿⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠟⢻⣿⣿⣿⣎⢙⣆⠀⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣷⡟⢻⠋⢸⡿⢾⣿⡦⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡦⠤⢾⣤⡼⠷⢿⡟⠁⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢸⠀⢷⠀⣸⢇⣤⠀⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⣤⣼⠀⠈⠓⠛⠉⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣆⢸⠀⠀⠀⠀⠀⠀")
-print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠀⠀⠀⠀⠀")
 
+var age: Int? = nil
+
+
+while age == nil {
+    print("Enter your age:")
+    
+    if let input = readLine(), let enteredAge = Int(input) {
+        if enteredAge < 0 {
+            print("Age cannot be negative. Enter a valid age.")
+        } else if enteredAge < 18 {
+            print("You must be 18 or older. Enter a valid age.")
+        } else {
+            age = enteredAge
+        }
+    } else {
+        print("Invalid input. Enter a valid integer for age.")
     }}
 
+
+print("Valid age entered: \(age!)")
+    }}
