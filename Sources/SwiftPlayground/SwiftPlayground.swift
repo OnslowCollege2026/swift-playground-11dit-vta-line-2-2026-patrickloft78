fc00a054fd5b7632ad086e67c59abb82a37442b7
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-05-26 21:10:22 +0000

    May 26, 2026, 9:10 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -8,7 +8,7 @@ struct SwiftPlayground {
 print("Price per item: $19.99")
 print("Quantity: 3.0")
 print("Subtotal: $\(19.99 * 3.0)")
-print("Tax (15%): $\(19.99 * 3.0 * 0.15)")
+print("Tax (15%): $\(19.99 * 3.0 * 0.15)") 
 print("Total: $\(19.99 * 3.0 * 1.15)")
 
     }}
