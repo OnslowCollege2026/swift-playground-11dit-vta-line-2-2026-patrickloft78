4fff4c4c58282040e019f4a1aa6ed83f18c47afc
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-06-21 22:15:48 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,10 +1,13 @@
+
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
 @main
 struct SwiftPlayground {
     static func main() {
-let total = 17.5 + 8.0 + 12.0
-print("Total cost is: $\(total)")
-
+func input(prompt: String, from: Int, to: Int -> Int {
+while true{
+    print(prompt, terminator:" ")
+}
+})
     }}
