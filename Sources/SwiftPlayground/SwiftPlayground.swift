fbd0e05f78f3e3ef0c2b654456995dcb0f52f635
patrickloft78 <patrick.loft@student.onslow.school.nz>
2026-06-25 01:30:01 +0000

    Jun 25, 2026, 1:30 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,39 +5,27 @@
 @main
 struct SwiftPlayground {
     static func main() {
-// this string allows the days to be entered into terminal automatically
 let days: [String] = ["monday","tuesday","wednesday","thurssday","friday","saturday","sunday",]
+let activities: [String] = ["instagram","whatsapp","tiktok","youtube","facebook","safari",]
+print("As your screentime is to high we will record it over six variables being")
+print("instagram, whatsapp, tiktok, youtube, safari, and facebook")
 
-print ("welcome to the sleep tracker")
-
-var isRunning = true
+var isRunning = true 
 if isRunning == true{
-    print ("Let's start by recording your sleep.")
-var totalSleepTime: Int = 0
-//this code makes the integer days paste monday tuesday etc
-days.forEach {day in
+    print("Lets start recording your screen time")
+    var totalScreenTime: Int = 0
+    days.forEach {day in
 var hoursRunning = true 
 while hoursRunning {
-    print("how many hours did you sleep on \(day)? (0-15)")
-    // If the the sleep time entered is inbtween a valid range 
-    if let userInput = readLine(), let sleepTime = Int(userInput), sleepTime >= 0 && sleepTime <= 15{
+
+    print("How much total screen time did you have on \(day)? (0-5)")
+    if let userInput = readLine(), let screenTime = Int(userInput), screenTime >= 0 && screenTime <= 24{
         
-        print("added \(sleepTime) Hours")
-        totalSleepTime += sleepTime
+        print("added \(screenTime) Hours")
+        totalScreenTime += screenTime
         hoursRunning = false  
 }else{
-    
-    print("please enter a valid interger ")
-}
-    
-}
-}
-let averageSleep = totalSleepTime / (days.count)
-// this tells us the total sleep time and the average 
-print("this week you slept a total of \(totalSleepTime) hours, with an average of \(averageSleep) hours per day.")
+print("Please enter a valid interger")
 
-}
 
-}
-
-    }
+}}}}}}
